d72d3ae6499ad3168c69ddd41b1ec0fc270103d5
Phillip <pwg1994@gmail.com>
2017-06-27 14:46:39 -0400

    minor changes

---
+++ SQLConnector/Connector/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String args[]){
+		//System.out.println("Hello world");
+		SQLConnector connection = new SQLConnector(); 
+		connection.DisplayUserData(); 
+		connection.DisplayBookData();
+		connection.DislpayBooksXML();
+	}
+}
