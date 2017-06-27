70a4313724e610f899333ffb4cec5e8c40919826
Phillip <pwg1994@gmail.com>
2017-06-26 20:04:41 -0400

    ignore

--- Verification.java
+++ Verification.java
@@ -4,37 +4,12 @@ import javax.mail.internet.*;
 import javax.activation.*;
 
 public class Verification {
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	   public static void main(String [] args) {
 		 //Generate verification code 
 	         Random randomNum = new Random(); 
 	         int verificationCode = randomNum.nextInt(999999); 
 	  
-	         System.out.println("CODE: " + verificationCode);
-		 
-	         
-	         
-	         
-	         
-	         
-	         
-	         
-	         
-	         
-	         
-		   
+	         System.out.println("CODE: " + verificationCode);   
 	   }
 	}
 
